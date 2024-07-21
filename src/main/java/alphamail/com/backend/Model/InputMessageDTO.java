b3bed3c5d6734bb8dd9bfa724447a85ec7ffd991
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-21 19:31:26 +0900

    feat : InputMessageDTO 클래스 추가 대학생 메일 양식의 입력값을 나타내는 클래스로, sender : 수신자 receiver : 송신자 department : 학과 studentId : 학번 subject : 과목 String content : 요구사항(내용) 이 포함된디.

--- src/main/java/alphamail/com/backend/Model/InputMessageDTO.java
+++ src/main/java/alphamail/com/backend/Model/InputMessageDTO.java
@@ -1,2 +1,9 @@
-package alphamail.com.backend.Model;public class InputMessageDTO {
+package alphamail.com.backend.Model;
+
+public record InputMessageDTO(String sender,
+                              String receiver,
+                              String department,
+                              String studentId,
+                              String subject,
+                              String content) {
 }
