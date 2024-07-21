6cd7c921762c9c285e2c3db66f4167d1dbd5bbeb
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-21 22:16:00 +0900

    feat : 제목 및 내용 구별하여 OutputMessageDTO 클래스로 리턴하도록 변경

--- src/main/java/alphamail/com/backend/university/service/UniversityService.java
+++ src/main/java/alphamail/com/backend/university/service/UniversityService.java
@@ -12,6 +12,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
 
 @Service
 public class UniversityService {
@@ -39,12 +40,26 @@ public class UniversityService {
         //보낼 데이터 정의
         String contentData = "";
 
+        //프론트엔드에서 데이터를 공백으로 입력할 경우 포함하지 않기 위해 if문 추가
         if(!inputMessageDTO.sender().isEmpty()) contentData = contentData.concat("이름 : " + inputMessageDTO.sender()+"\n");
         if(!inputMessageDTO.receiver().isEmpty()) contentData = contentData.concat("교수님 성함 : " + inputMessageDTO.receiver()+"\n");
         if(!inputMessageDTO.department().isEmpty()) contentData = contentData.concat("학부 : " + inputMessageDTO.department()+"\n");
         if(!inputMessageDTO.studentId().isEmpty()) contentData = contentData.concat("학번 : " + inputMessageDTO.studentId()+"\n");
         if(!inputMessageDTO.subject().isEmpty()) contentData = contentData.concat("과목명 : " + inputMessageDTO.subject()+"\n");
         if(!inputMessageDTO.content().isEmpty()) contentData = contentData.concat(inputMessageDTO.content()+"\n");
+        //title과 content 분리의 편의성 위해 추가
+        contentData = contentData.concat("제목 앞에는 '제목 : '을 붙여 주고, 제목과 내용은 엔터로 구분지어 주세요.");
+
+        //범위 : 0 ~ 1, 선택 확률 값이 높은 토큰을 순서대로 나열한 후 설정한 누적 확률 값에 포함되지 않는 토큰을 제거할 때 사용하는 기준값, 보통 0.8 ~ 1로 설정
+        double topP = 0.8;
+        //범위 : 0 ~ 128, 확률 값이 가장 높은 K개 중에서 하나를 선택할 때 사용하는 기준 값, 특수한 경우 아닐 경우 0으로 설정
+        int topK = 0;
+        //범위 : 0 ~ 4096, Maximum tokens는 결괏값을 생성할 때 사용할 최대 토큰 수, 토큰 수를 높게 설정할 수록 긴 결괏값을 출력, 보통 300 ~ 500으로 설정
+        int maxTokens = 350;
+        //범위 : 0 ~ 1, 낮으면 정형화된 응답, 높으면 다양한 문장을 만들지만 문장의 질이 떨어질 수 있음, Top P를 고정한 채로 변경하는 것을 추천,
+        double temperature = 0.7;
+        //범위 : 0 ~ 10, 반복적인 결괏값을 생성하지 않도록 반복되는 토큰에는 감점 요소를 부여하는 값
+        double repeatPenalty = 5;
 
         String inputData = String.format("""
                 {
@@ -55,15 +70,15 @@ public class UniversityService {
                     "role" : "user",
                     "content" : "%s"
                   } ],
-                  "topP" : 0.8,
-                  "topK" : 0,
-                  "maxTokens" : 256,
-                  "temperature" : 0.5,
-                  "repeatPenalty" : 5.0,
+                  "topP" : %f,
+                  "topK" : %d,
+                  "maxTokens" : %d,
+                  "temperature" : %f,
+                  "repeatPenalty" : %f,
                   "stopBefore" : [ ],
                   "includeAiFilters" : true,
                   "seed" : 0
-                }""", contentData);
+                }""", contentData, topP, topK, maxTokens, temperature, repeatPenalty);
 
         //데이터 전송
         try(OutputStream os = connection.getOutputStream()) {
@@ -81,9 +96,20 @@ public class UniversityService {
         in.close(); // BufferedReader를 닫아 리소스를 해제
 
         JSONObject jsonObject = new JSONObject(response.toString());
-        String content = jsonObject.getJSONObject("result").getJSONObject("message").getString("content");
+        String result = jsonObject.getJSONObject("result").getJSONObject("message").getString("content");
+        
+        //title과 content '\n'으로 분리
+        StringTokenizer st = new StringTokenizer(result,"\n");
+        //title의 경우 '제목 : '을 제거하기 위해 substring(5) 로 지정
+        String title = st.nextToken().substring(5);
 
-        return new OutputMessageDTO(contentData ,content);
+        //content 내용 추가
+        String content = "";
+        while(st.hasMoreTokens()) {
+            content = content.concat(st.nextToken() + "\n");
+        }
+
+        return new OutputMessageDTO(title ,content);
     }
 }
 
