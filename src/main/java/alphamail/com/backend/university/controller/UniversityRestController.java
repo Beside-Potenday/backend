b87da48827904a7ddcceb40dff296445049831ee
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-26 17:28:16 +0900

    feat : 테스트용 post요청 제거

--- src/main/java/alphamail/com/backend/university/controller/UniversityRestController.java
+++ src/main/java/alphamail/com/backend/university/controller/UniversityRestController.java
@@ -1,7 +1,7 @@
 package alphamail.com.backend.university.controller;
 
-import alphamail.com.backend.university.model.InputMessageDTO;
-import alphamail.com.backend.university.model.OutputMessageDTO;
+import alphamail.com.backend.university.model.UniversityRequest;
+import alphamail.com.backend.university.model.UniversityResponse;
 import alphamail.com.backend.university.service.UniversityService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,23 +15,9 @@ public class UniversityRestController {
         this.universityService = universityService;
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<?> test() throws Exception{
-        InputMessageDTO inputMessageDTO = new InputMessageDTO("곽두팔",
-                "",
-                "컴퓨터학부",
-                "2020000000",
-                "대화형 프로그래밍 기초",
-                "교수님꼐 성적 정정을 요청하는 메일을 작성해주세요.");
-
-        OutputMessageDTO response = universityService.getResult(inputMessageDTO);
-
-        return ResponseEntity.ok(response);
-    }
-
     @PostMapping("/univ")
-    public ResponseEntity<?> getUniv(@RequestBody InputMessageDTO inputMessageDTO) throws Exception{
-        OutputMessageDTO response = universityService.getResult(inputMessageDTO);
+    public ResponseEntity<?> getUniv(@RequestBody UniversityRequest universityRequest) throws Exception{
+        UniversityResponse response = universityService.getResult(universityRequest);
 
         return ResponseEntity.ok(response);
     }
