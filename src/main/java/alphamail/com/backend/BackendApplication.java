d70833c8b5307e2f4edf619e44b37a23af3f78ef
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-28 00:24:23 +0900

    refactor : 수정

--- src/main/java/alphamail/com/backend/BackendApplication.java
+++ src/main/java/alphamail/com/backend/BackendApplication.java
@@ -18,7 +18,10 @@ public class BackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("*");
 			}
 		};
 	}
