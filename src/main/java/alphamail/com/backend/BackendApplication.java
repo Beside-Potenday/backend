33d57728e7756dbb896b6bd545b629c7fbabf150
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-27 23:57:37 +0900

    refactor : 복구

--- src/main/java/alphamail/com/backend/BackendApplication.java
+++ src/main/java/alphamail/com/backend/BackendApplication.java
@@ -2,23 +2,11 @@ package alphamail.com.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("http_route", r -> r.path("/api/**")
-						.uri("https://localhost:443"))
-				.build();
 	}
 }
