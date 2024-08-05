7bea48af2c3951c6282615fa8cfd69fad293ac8a
2iedo <162278702+2iedo@users.noreply.github.com>
2024-08-05 23:59:50 +0900

    feat : 각 컬럼 별 세부사항 추가

--- src/main/java/alphamail/com/backend/user/entity/MemberEntity.java
+++ src/main/java/alphamail/com/backend/user/entity/MemberEntity.java
@@ -1,30 +1,29 @@
 package alphamail.com.backend.user.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-public class memberEntity {
+@Entity(name = "member")
+public class MemberEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column
+    @Column(name = "email", nullable = false)
     private String email;
 
-    @Column
+    @Column(name = "profileUrl", nullable = false)
     private String profileUrl;
 
-    public memberEntity(){}
+    public MemberEntity(){}
 
-    public memberEntity(String name, String email, String profileUrl){
+    public MemberEntity(String name, String email, String profileUrl){
         this.name = name;
         this.email = email;
         this.profileUrl = profileUrl;
