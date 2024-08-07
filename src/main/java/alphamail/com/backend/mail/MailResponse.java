d8e518bef9d09043216b71fa7fa48ff0abc5379a
2iedo <162278702+2iedo@users.noreply.github.com>
2024-08-07 19:13:50 +0900

    refactor : response에 createDate 추가

--- src/main/java/alphamail/com/backend/mail/MailResponse.java
+++ src/main/java/alphamail/com/backend/mail/MailResponse.java
@@ -2,11 +2,14 @@ package alphamail.com.backend.mail;
 
 import alphamail.com.backend.mail.model.MailEntity;
 
+import java.time.LocalDateTime;
+
 public record MailResponse(String subject,
-                           String body) {
+                           String body,
+                           LocalDateTime createDate) {
 
     public static MailResponse from(MailEntity mail) {
         return new MailResponse(mail.getSubject(),
-            mail.getBody());
+            mail.getBody(), mail.getCreatedDate());
     }
 }
